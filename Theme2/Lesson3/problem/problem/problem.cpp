afac73775a685d43c6c8ce72009d539052a8c22d
Maksim D <dbimzer93@gmail.com>
2025-11-15 11:34:04 +0300

    добавление решение 7 урока в 1 теме и 3 урока в 2 теме

---
+++ Theme2/Lesson3/problem/problem/problem.cpp
@@ -0,0 +1,31 @@
+﻿#include <cstdint>
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    int64_t a;
+    int64_t b;
+    cin >> a >> b;
+
+    if (a > 0 && b > 0) {
+        if (a > LLONG_MAX - b) {
+            cout << "Overflow!" << endl;
+        }
+        else {
+            cout << a + b << endl;
+        }
+    }
+    else if (a < 0 && b < 0) {
+        if (a < LLONG_MIN - b) {
+            cout << "Overflow!" << endl;
+        }
+        else {
+            cout << a + b << endl;
+        }
+    }
+    else {
+        cout << a + b << endl;
+    }
+}
